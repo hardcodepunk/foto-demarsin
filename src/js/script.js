8f48a27e769c510568487cdd50a60ee4729d2320
friederjanmoerman <hijbrid@gmail.com>
2018-08-28 17:57:11 +0200

    created the studio sub page

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("site-nav").style.transform = "translate(0,0)"
+  } else {
+    document.getElementById("site-nav").style.transform = "translate(0,-115px)"
+  }
+  prevScrollpos = currentScrollPos;
+}
