ebfe5b2b1470159d921a15ef2beff0ff4038e484
friederjanmoerman <hijbrid@gmail.com>
2018-08-30 18:43:41 +0200

    made nav responsive

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,9 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+var toggleNav = document.querySelector('.js-menu-toggle');
+toggleNav.onclick = function() {
+  document.querySelector('.body').classList.toggle('open-menu');
+  console.log("yeah");
+}
